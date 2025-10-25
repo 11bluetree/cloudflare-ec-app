c89c86761c8d5a397adc85558d458d5aba467358
11bluetree <lax.bluetree@hotmail.co.jp>
2025-10-25 23:24:58 +0900

    feat: Tailwind CSSをプロジェクトに統合し、設定ファイルを追加 - muiは直感で使いにくかったので移行する

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,6 @@ export default defineConfig({
     // TanStack Routerプラグインはreactプラグインの前に配置する必要がある
     tanstackRouter(),
     react(),
+    tailwindcss(),
   ],
 })
